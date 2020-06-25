d20b58b3bf60c7656d389a49f44828db57812b84
kevin-salamon <kevinwebdevs@gmail.com>
2020-06-25 11:05:52 -0400

    test calculator object complete

--- Test.java
+++ Test.java
@@ -1,6 +1,24 @@
-class Test {
+public class Test {
+
+    public double calcTest(double int1, String operator, double int2) {
+
+        if (operator == "+") {
+            return int1 + int2;
+        } else if (operator == "-") {
+            return int1 - int2;
+        } else if (operator == "*") {
+            return int1 * int2;
+        } else if (operator == "/") {
+            return int1 / int2;
+        } else {
+            return -1;
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        Test myObj = new Test();
+        double test = myObj.calcTest(1, "-", 2);
+        System.out.println(test);
     }
 }
